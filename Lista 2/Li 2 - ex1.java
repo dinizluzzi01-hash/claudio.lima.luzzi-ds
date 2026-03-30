d485a4c19c381fd61b75dd3a1090975d2017782e
claudiolimaluzzi-hash <dinizluzzi01@gmail.com>
2026-03-29 22:12:20 -0300

    Add files via upload

---
+++ Lista 2/Li 2 - ex1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("Dobro: " + (n * 2));
+        System.out.println("Triplo: " + (n * 3));
+    }
+}
