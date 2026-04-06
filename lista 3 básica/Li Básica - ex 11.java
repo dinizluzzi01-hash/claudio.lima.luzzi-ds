8403e4158d8c19ccdb13f249887747b962c1642f
claudiolimaluzzi-hash <dinizluzzi01@gmail.com>
2026-04-06 09:11:36 -0300

    Lista 3 - básico

---
+++ lista 3 básica/Li Básica - ex 11.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    int myAge = 25;
+    int votingAge = 18;
+    
+    if (myAge >= votingAge) {
+      System.out.println("Você é maior de idade ");
+    } else {
+      System.out.println("Você não é maior de idade.");
+    }  
+  }
+}
