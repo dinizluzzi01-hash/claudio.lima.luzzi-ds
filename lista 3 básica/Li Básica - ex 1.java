1e3632c084d9c8d07ab077327c1237d8fd85ccc5
claudiolimaluzzi-hash <dinizluzzi01@gmail.com>
2026-04-23 11:05:58 -0300

    lista 3 - Básica

---
+++ lista 3 básica/Li Básica - ex 1.java
@@ -0,0 +1,7 @@
+int n = 5;
+int i = 1;
+
+while (i <= n) {
+    System.out.println(i);
+    i++;
+}
---
+++ lista 3 básica/Li Básica - ex 3.java
@@ -0,0 +1,9 @@
+int num = 1;
+int soma = 0;
+
+while (num != 0) {
+    soma += num;
+    num--; // simulação
+}
+
+System.out.println("Soma: " + soma);
