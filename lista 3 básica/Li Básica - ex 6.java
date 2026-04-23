8931cd404e7919cfd2635fec8de12eab74ad8289
claudiolimaluzzi-hash <dinizluzzi01@gmail.com>
2026-04-23 11:11:01 -0300

    Lista 3 - Básica

---
+++ lista 3 básica/Li Básica - ex 6.java
@@ -0,0 +1,9 @@
+int opcao = 1;
+
+while (opcao != 0) {
+    switch (opcao) {
+        case 1: System.out.println("Opção 1"); break;
+        case 0: System.out.println("Saindo..."); break;
+    }
+    opcao--; // simulação
+}
---
+++ lista 3 básica/Li Básica - ex 7.java
@@ -0,0 +1,7 @@
+int n = 10;
+
+for (int i = 0; i <= n; i++) {
+    if (i % 2 == 0) {
+        System.out.println(i);
+    }
+}
